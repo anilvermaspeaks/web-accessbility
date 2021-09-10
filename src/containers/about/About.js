dd68af6575d5378832149195692faf7539ab6be8
anil verma <anilcs240@gmail.com>
2021-09-10 17:47:52 +0530

    added few exe

--- src/containers/about/About.js
+++ src/containers/about/About.js
@@ -1,12 +1,49 @@
 import classes from './About.module.css';
+import { vegetables } from '../../data.js'
 
 const About = () => {
     return (
         <div>
-            <h1>About</h1>
+            <section className={classes.contrastRadio}>
+                <h3>Contrast Radio</h3>
+                <p>What is Lorem Ipsum Lorem Ipsum is simply dummy text of the printing and typesetting industry Lorem Ipsum has been the industry's standard dummy text ever since the 1500s when an unknown printer took a galley of type and scrambled it to make a type specimen book it has?
+                </p>
+            </section>
+
+            <section className={classes.semanticEls}>
+                <h3>Use of semantic tag</h3>
+                <div onClick={0}>Link item with div </div>
+                <div><a href="/">Link item with anchor</a></div>
+                <button onClick={0}>Link item with button</button>
+            </section>
+
+            <section className={classes.grocerySec}>
+                <h3>Proper labeling while listing data</h3>
+                <ul>{vegetables && vegetables.map((vegetable) => <li key={vegetable.id}>
+                    <b>{vegetable.name}</b>
+                    <div> <img src={vegetable.photo_url} alt="" /></div>
+                    <button onClick={0}>Buy</button>
+                </li>)}</ul>
+            </section>
+
+            <section className={classes.enoughTime}>
+                <h3>Give enough Time</h3>
+                <p>Complete this form in 10 min.</p>
+            </section>
+
+            <section className={classes.enoughTime}>
+                <h3>Avoid Jargon</h3>
+                <p>Use simple lang.</p>
+                <abbr title="Indian Space Research Organisation">ISRO</abbr>
+            </section>
+
+            <section className={classes.enoughTime}>
+                <h3>Always Define Lang attribute on html tag</h3>
+                <p>Toujours définir l'attribut Lang sur la balise html</p>
+                <p lang="fr">Toujours définir l'attribut Lang sur la balise html</p>
+            </section>
         </div>
     )
 }
-
 
 export default About;
---
+++ src/data.js
@@ -0,0 +1,29 @@
+export const vegetables = [
+	{
+		"name": "Cabbage",
+		"photo_url": "https://www.freepngimg.com/thumb/categories/2970.png",
+		"quantity": "One Unit",
+		"id": 2,
+		"price": 30
+	},
+	{
+		"name": "Capsicum",
+		"photo_url": "https://www.nicepng.com/png/detail/52-525615_green-bell-pepper-png-green-capsicum-png.png",
+		"quantity": "One Unit",
+		"id": 7,
+		"price": 5
+	},
+	{
+		"name": "Garlic",
+		"photo_url": "https://www.freepngimg.com/thumb/garlic/2-2-garlic-transparent-thumb.png",
+		"quantity": "One Unit",
+		"id": 10,
+		"price": 20
+	},
+	{
+		"name": "Beetroot",
+		"photo_url": "https://pngimg.com/uploads/beet/beet_PNG28.png",
+		"quantity": "One Unit",
+		"id": 11,
+		"price": 20
+	}]
