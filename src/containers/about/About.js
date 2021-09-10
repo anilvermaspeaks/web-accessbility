3ffc0dec8a1be0d3f5d14b667a72e1b71aad9e53
anil verma <anilcs240@gmail.com>
2021-09-10 20:51:45 +0530

    added few more usecases for form control

--- src/containers/about/About.js
+++ src/containers/about/About.js
@@ -2,6 +2,9 @@ import classes from './About.module.css';
 import { vegetables } from '../../data.js'
 
 const About = () => {
+    const alertFn = () => {
+        alert()
+    }
     return (
         <div>
             <section className={classes.contrastRadio}>
@@ -13,8 +16,9 @@ const About = () => {
             <section className={classes.semanticEls}>
                 <h3>Use of semantic tag</h3>
                 <div onClick={0}>Link item with div </div>
+                {/* aria-label="Take me to Google" */}
                 <div><a href="/">Link item with anchor</a></div>
-                <button onClick={0}>Link item with button</button>
+                <button onClick={alertFn}>Link item with button</button>
             </section>
 
             <section className={classes.grocerySec}>
@@ -34,15 +38,16 @@ const About = () => {
             <section className={classes.enoughTime}>
                 <h3>Avoid Jargon</h3>
                 <p>Use simple lang.</p>
+                {/* //aria-label="Indian Space Research Organisation" */}
                 <abbr title="Indian Space Research Organisation">ISRO</abbr>
             </section>
 
-            <section className={classes.enoughTime}>
-                <h3>Always Define Lang attribute on html tag</h3>
-                <p>Toujours définir l'attribut Lang sur la balise html</p>
-                <p lang="fr">Toujours définir l'attribut Lang sur la balise html</p>
-            </section>
-        </div>
+            <section className={classes.AriaRoles} >
+                <h3>Use Aria role Attribute to indicate Genric Tag behaving like some Standard tag</h3>
+                <div tabIndex="0" role="button">Click Me</div>
+                <div tabIndex="0" role="search">Search here</div>
+            </section >
+        </div >
     )
 }
 
--- src/containers/contact/Contact.js
+++ src/containers/contact/Contact.js
@@ -4,7 +4,15 @@ import classes from './Contact.module.css';
 const Contact = () => {
     return (
         <div>
-            <h1>Contact</h1>
+            <section className={classes.labelControls}>
+                <h3>Label Your Controles</h3>
+                <div><label>User Name</label>
+                    <input type="text" /></div>
+                <div><label for="UserEmail">Email</label>
+                    <input type="text" id="UserEmail" /></div>
+                <div><label for="UserPassword">Enter User Password</label>
+                    <input type="password" id="UserPassword" /></div>
+            </section>
         </div>
     )
 }
